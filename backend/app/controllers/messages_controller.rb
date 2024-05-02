b8add65f8d2450afe683dd310b58356152987643
LorDEvilDead <lordevildead@gmail.com>
2024-05-02 20:07:13 +0300

    resolved conversation

--- backend/app/controllers/messages_controller.rb
+++ backend/app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 # app/controllers/messages_controller.rb
 class MessagesController < ApplicationController
-    def index
-     render json: { message: 'Hello, World from Rails!' }
-    end
+  def index
+    render json: { message: 'Hello, World from Rails!' }
   end
-  
+end
