8df3ea9c73b98ef449bfe2580fd5f49068dbaa36
LorDEvilDead <lordevildead@gmail.com>
2024-05-07 13:44:15 +0300

    i think i do something wrong, need sup

--- backend/app/controllers/products_controller.rb
+++ backend/app/controllers/products_controller.rb
@@ -1,9 +1,22 @@
 class ProductsController < ApplicationController
-    def new
+    include Dry::Monads::Result::Mixin
+
+    def call(params:, owner:)
+        @params = params
+        @user = user
+        cteate_product
     end
 
     def create
+        case product_creation
+        in Success(product)
+          render json: prepare_json(product), status: :created
+        in Failure(error)
+          render json: { errors: error.messages }, status: :unprocessable_entity
+        end
     end
+
+ 
     
     def show
         id = params.extract_value(:id)
@@ -11,4 +24,26 @@ class ProductsController < ApplicationController
     end
     
     private
+    attr_reader :params, :owner
+    
+    def create_product
+        product.owner_id = owner.id
+        product.save ? Success(product) : Failure(product.errors)
+    end
+
+    def product
+        @product ||= Product.new(params)
+    end
+
+    def product_creation
+    Products::Create.new.call(params: product_params)
+    end
+
+    def product_params
+        params.rquire(name: product_name).permit(price: product_price, 
+        weight: product_weight, 
+        consist: product_consist,
+        disription: product_disription,
+        photo: product_photo)
+    end
 end
