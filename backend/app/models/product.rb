94cc9ec0a4b3cc7a9c372b6259576db5c626dcab
Кудрявцев Павел <kudravcevpavel@Kudravcevs-MacBook-Air.local>
2024-05-15 16:06:38 +0300

    added methods:  index, update, destroy, set

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
-    validates :name, presence: true, lenght: {maximum: 100}
+  validates :name, presence: true, lenght: { maximum: 100 }
 end
