1301f1f217b58351e3db773debc4811fed2c262a
Кудрявцев Павел <kudravcevpavel@Kudravcevs-MacBook-Air.local>
2024-05-23 16:54:13 +0300

    deleted monads

--- backend/app/controllers/products_controller.rb
+++ backend/app/controllers/products_controller.rb
@@ -1,65 +1,48 @@
 class ProductsController < ApplicationController
-    include Dry::Monads::Result::Mixin
-
-    #POST /products
-    def create
-        case product_creation
-        in Success(product)
-          render json: prepare_json(product), status: :created
-        in Failure(error)
-          render json: { errors: error.messages }, status: :unprocessable_entity
-        end
-    end
-
-    #PUT/PATCH /products/edit/1
-    def update
-        case set_product
-        in Success(product)
-            render json: prepare_json(product), status :updated
-        in Failure
-            render json: { error: error.messages }, status :unprocessable_entity
-        end
-    end
-
-    #GET /products/1
-    def show
-        id = params.extract_value(:id)
-        @product = product.find(id)
-    end
 
     #GET /products
     def index
         @products = Product.all
         
-        render json @products
+        render json: @products
     end
-    
+
+    #GET /products/1
+    def show 
+        render json: @product
+    end
+
+    #POST /products
+    def create 
+        @product = Product.new(product_params)
+        if @product.save
+            render json: @gproduct, status: :created, location: @product
+        else 
+            render json: @product.errors, status: :unprocessable_entity
+        end
+    end
+
+    #PUT/PATCH /products/1
+    def update
+        if @product.update(product_params)
+            render json: @product
+        else 
+            render json: @product.errors, status: :unprocessable_entity
+        end
+    end
+
     #DELETE /products/1
     def destroy
-    @product.destroy
+        @product.destroy
     end
-    
+
     private
-    #not sure about that
-    attr_reader :params, :user
-
-    # Use callbacks to share common setup or constraints between actions.
+    #Use callbacks to share common setup or constrains between actions.
     def set_product
-        @product = Product.find(product_params[:id])
+        @product = product.find(params[:id])
     end
 
-    #call monades
-    def product_creation
-    Products::Create.new.call(params: product_params)
+    #only allow a list of trusted parameters though.
+    def group_params
+        params.require(:product).permit(:price, :portion_weight_grams, :ingredients, :desription, :image, :name)
     end
-
-     # Only allow a list of trusted parameters through.
-    def product_params
-        params.require(name: product_name).permit(id: product_id,
-        price: product_price, 
-        portion_weight_grams: product_weight, 
-        ingredients: product_ingredients,
-        desription: product_desription,
-        image: product_image)
-    end
-end
