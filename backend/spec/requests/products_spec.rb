478793c1013e52917e4c2a92f79865569965415e
Кудрявцев Павел <kudravcevpavel@Kudravcevs-MacBook-Air.local>
2024-06-04 16:08:26 +0300

    fix controller and added tests to model and get, post tests to controller

--- backend/spec/requests/products_spec.rb
+++ backend/spec/requests/products_spec.rb
@@ -2,8 +2,38 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Products' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+# RSpec.describe 'Products' do
+#   describe 'GET /index' do
+#     pending "add some examples (or delete) #{__FILE__}"
+#   end
+# end
+
+RSpec.describe ProductsController, type: :controller do
+  describe "GET /index" do
+    it "returns a success response" do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  # describe "GET #show" do
+  #   it "returns a success response" do
+  #     get :show
+  #     expect(response).to be_successful
+  #   end
+  # end
+
+  describe "POST #create" do
+    it "creates a new Product" do
+      expect {
+        post :create, params: { product: { name: 'Cake with cherry' } }
+      }.to change(Product, :count).by(1)
+    end
+
+    # it "PUT Product #update" do 
+    # it "updates the Product" do 
+    #     expect {
+    #         put :update, params { product: {id: 1}}
+    #     }.to change(Product, :name)
   end
 end
